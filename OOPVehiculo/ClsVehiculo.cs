b559aff5ac71286343080ef5e750291958d607b6
IsabellaP23 <isabella.pena473@pascualbravo.edu.co>
2025-04-07 18:16:15 -0500

    Add files via upload

---
+++ OOPVehiculo/ClsVehiculo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class ClsVehiculo
+    {
+        public string Matricula { get; set; }
+        public string Modelo { get; set; }
+        public string PotenciaCV { get; set; }
+
+        public void getMatricula()
+        {
+            Console.WriteLine("Ingrese la matricula del vehiculo");
+            Matricula = Console.ReadLine();
+        }
+        public void getModelo()
+        {
+            Console.WriteLine("Ingrese el modelo de su vehiculo");
+            Modelo = Console.ReadLine();
+        }
+        public void getPotenciaCV()
+        {
+            Console.WriteLine("Ingrese la potencia de su vehiculo");
+            PotenciaCV = Console.ReadLine();
+        }
+
+    }
+    public class ClsTaxi : ClsVehiculo
+    {
+        public string numeroLicencia { get; set; }
+        public void setNumeroLicencia(string Licencia)
+        {
+            numeroLicencia = Licencia;
+        }
+        public void getNumeroLicencia() 
+        {
+            Console.WriteLine("Ingrese el número de licencia de su Taxi");
+            numeroLicencia = Console.ReadLine();
+        }
+    }
+    public class ClsAutobus : ClsVehiculo
+    {
+        public string numeroPlazas { get; set; }
+        public void setNumeroPlazas(string plazas)
+        {
+            numeroPlazas = plazas;
+        }
+        public void getNumeroPlazas()
+        {
+            Console.WriteLine("Ingrese el número de plazas de su autobus");
+            numeroPlazas = Console.ReadLine();
+        }
+    }
+}
---
+++ OOPVehiculo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ClsVehiculo> vehiculo = new List<ClsVehiculo>();
+
+            bool salir = false;
+            while (!salir)
+            {
+                Console.WriteLine("Seleccione su tipo de vehiculo \n\n1.Taxi \n2.Autobus \n3.salir");
+                int opc = int.Parse(Console.ReadLine());
+                
+                switch (opc)
+                {
+                    case 1:
+                        ClsTaxi taxi = new ClsTaxi();
+
+                        taxi.getMatricula();
+                        taxi.getModelo();
+                        taxi.getPotenciaCV();
+                        taxi.getNumeroLicencia();
+                        vehiculo.Add(taxi);
+                        Console.Clear();
+                        break;
+                    case 2:
+                        ClsAutobus autobus = new ClsAutobus();
+
+                        autobus.getMatricula();
+                        autobus.getModelo();
+                        autobus.getPotenciaCV();
+                        autobus.getNumeroPlazas();
+                        vehiculo.Add(autobus);
+                        Console.Clear();
+                        break;
+                    case 3:
+                        salir = true;
+                        Console.Clear();
+                    break;
+
+                    default:
+                        Console.WriteLine("Opción Invalida");
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nVehículos registrados:");
+            foreach (ClsVehiculo item in vehiculo)
+            {
+                if (item is ClsTaxi taxi)
+                {
+                    Console.WriteLine("Tipo de Vehículo: Taxi");
+                    Console.WriteLine($"Matrícula: {taxi.Matricula}");
+                    Console.WriteLine($"Modelo: {taxi.Modelo}");
+                    Console.WriteLine($"Potencia: {taxi.PotenciaCV}");
+                    Console.WriteLine($"Número de licencia: {taxi.numeroLicencia}");
+                }
+                else if (item is ClsAutobus autobus)
+                {
+                    Console.WriteLine("Tipo de Vehículo: Autobús");
+                    Console.WriteLine($"Matrícula: {autobus.Matricula}");
+                    Console.WriteLine($"Modelo: {autobus.Modelo}");
+                    Console.WriteLine($"Potencia: {autobus.PotenciaCV}");
+                    Console.WriteLine($"Número de plazas: {autobus.numeroPlazas}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
